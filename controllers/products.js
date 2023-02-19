d95ec5fbbf9f70ce8dcdaf68c5a4fa2fff3a1084
darimtiyaz <darimtiyaz193@gmail.com>
2023-02-19 16:13:03 +0530

    node resetApi

---
+++ controllers/products.js
@@ -0,0 +1,12 @@
+const Product = require("../models/product");
+
+const getAllProducts = async(req, res)=> {
+  const myData = await Product.find({});
+  res.status(200).json({myData});
+};
+
+const getAllProductsTesting = async(req, res)=> {
+  res.status(200).json({msg: "i am getAllProductsTesting"});
+};
+
+module.exports = { getAllProducts, getAllProductsTesting };
